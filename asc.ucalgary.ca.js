e96704dbe3e102815cc121025402e2f24ebf2c35
Damian Bauder <drbauder@users.noreply.github.com>
2024-06-21 10:18:35 -0600

    Create asc.ucalgary.ca.js

---
+++ asc.ucalgary.ca.js
@@ -0,0 +1,23 @@
+const lcrEnvironment = "Development";
+const lcrBootstrapHost = "https://localhost:5443";
+
+Blazor.start({
+  environment: lcrEnvironment,
+  loadBootResource: function (type, name, defaultUri, integrity) {
+    console.log(
+      `Loading: '${type}', '${name}', '${defaultUri}', '${integrity}'`
+    );
+    switch (type) {
+      case "assembly":
+      case "pdb":
+      case "dotnetjs":
+      case "dotnetwasm":
+      case "timezonedata":
+        return `${lcrBootstrapHost}/_framework/${name}`;
+    }
+  },
+}).then(function () {
+  let targetElement = document.getElementById("news-container");
+  if (targetElement !== null)
+    Blazor.rootComponents.add(targetElement, "news-component", {});
+});
